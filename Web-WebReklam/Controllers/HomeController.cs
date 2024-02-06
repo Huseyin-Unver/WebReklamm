32f821ea2448e8643a6ed94ac0a6e4ceec5f3e2a
Furkanoku06 <furkanoku06@gmail.com>
2024-02-06 13:45:07 +0300

    form

--- Web-WebReklam/Controllers/HomeController.cs
+++ Web-WebReklam/Controllers/HomeController.cs
@@ -33,5 +33,12 @@ namespace Web_WebReklam.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
+        public IActionResult RequestForm()
+        {
+
+            return View(); 
+        }
+
     }
 }
